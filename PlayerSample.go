c37abece6f0231e784a45c615ccf45188f9d2b6b
Ammar Bandukwala <ammar@ammar.io>
2016-02-19 17:42:18 -0600

    Document PlayerSample

--- PlayerSample.go
+++ PlayerSample.go
@@ -1,5 +1,7 @@
 package mcping
 
+
+//PlayerSample contains a player response in the sample section of a ping response.
 type PlayerSample struct {
 	UUID string //e.g "d8a973a5-4c0f-4af6-b1ea-0a76cd210cc5"
 	Name string //e.g "Ammar"
