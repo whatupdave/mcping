eb35cb59f5c50647d0649a5f57ce91bff03b316d
Ammar Bandukwala <ammar@ammar.io>
2016-02-19 23:03:06 -0600

    json tags

--- PingResponse.go
+++ PingResponse.go
@@ -1,14 +1,27 @@
 package mcping
 
+import (
+	"regexp"
+)
+
 //PingResponse contains all known fields of the ping response packet
 type PingResponse struct {
-	Latency  uint   //Latency in ms
-	Online   int    //Amount of online players
-	Max      int    //Maximum amount of players
-	Protocol int    //E.g '4'
-	Favicon  string //Base64 encoded favicon in data URI format
-	Motd     string
-	Server   string //E.g 'PaperSpigot'
-	Version  string //E.g "1.7.10"
-	Sample   []PlayerSample
+	Latency  uint           `json:"latency"`  //Latency in ms
+	Online   int            `json:"online"`   //Amount of online players
+	Max      int            `json:"max"`      //Maximum amount of players
+	Protocol int            `json:"protocol"` //E.g '4'
+	Favicon  string         `json:"favicon"`  //Base64 encoded favicon in data URI format
+	Motd     string         `json:"motd"`
+	Server   string         `json:"server"`  //E.g 'PaperSpigot'
+	Version  string         `json:"version"` //E.g "1.7.10"
+	Sample   []PlayerSample `json:"sample"`
+}
+
+//Code unknown to work, playing around
+func stripMotd(motd string) string {
+	return regexp.MustCompile("\\s+").ReplaceAllString(regexp.MustCompile("`+.").ReplaceAllString(
+		regexp.MustCompile("[^\x20-\x7f]").ReplaceAllString(
+			motd, "`"),
+		""),
+		" ")
 }
